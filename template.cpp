6b00b5de8df4ffb46599d46b2888e756607493c8
Łukasz Dąbek <sznurek@gmail.com>
2013-11-19 20:18:11 +0100

    Added template.

---
+++ template.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <cmath>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+typedef long long int ll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+
+int main() {
+
+    return 0;
+}
+
